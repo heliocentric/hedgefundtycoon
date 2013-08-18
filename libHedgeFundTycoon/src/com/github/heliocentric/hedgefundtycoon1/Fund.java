b2851b7e5c3583e7c2125491fe8dea932c665e1c
Dylan Cochran <heliocentric@gmail.com>
2013-08-18 16:53:37 -0400

    Add the primary library, and a placeholder for the gui.

---
+++ libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/Fund.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.hedgefundtycoon1;
+
+/**
+ *
+ * @author Helio
+ */
+public class Fund {
+	private char _karma;
+	private char _rating;
+	public Fund() {
+		this._karma = 128;
+		this._rating = 91;
+	}
+	public String Karma() {
+		String retval;
+		if (this._rating >= 192) {
+			retval = "Good";
+		} else if (this._rating >= 64){
+			retval = "Neutral";
+		} else {
+			retval = "Bad";
+		}
+		return retval;
+	}
+	public String Rating() {
+		String retval;
+		if (this._rating >= 101) {
+			retval = "Strong Buy";
+		} else if (this._rating >= 81){
+			retval = "Buy";
+		} else if (this._rating >= 41){
+			retval = "Hold";
+		} else if (this._rating >= 21){
+			retval = "Sell";
+		} else if (this._rating >= 1){
+			retval = "Strong Sell";
+		} else {
+			retval = "Bankrupt";
+		}
+		return retval;
+	}
+}
