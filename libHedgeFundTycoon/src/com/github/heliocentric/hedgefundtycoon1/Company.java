526d80bf77eb729798b45a8bcaf49e195809392b
Dylan Cochran <heliocentric@gmail.com>
2013-08-19 20:47:42 -0400

    Add Company class that implements the 'Agent' interface.

---
+++ libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/Company.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.hedgefundtycoon1;
+
+/**
+ *
+ * @author Helio
+ */
+public class Company implements Agent {
+	private Fundamental Current;
+	
+}
