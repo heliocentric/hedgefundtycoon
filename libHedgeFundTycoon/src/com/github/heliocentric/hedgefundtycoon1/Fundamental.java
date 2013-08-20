105dfa2239cf26e44288ea4a8033c375a6b3afb6
Dylan Cochran <heliocentric@gmail.com>
2013-08-19 20:23:51 -0400

    Add Currency and shares to Fundamental

--- libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/Fundamental.java
+++ libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/Fundamental.java
@@ -9,5 +9,21 @@ package com.github.heliocentric.hedgefundtycoon1;
  * @author Helio
  */
 public class Fundamental {
-	
+	private Unit _currency;
+	public Unit getCurrency() {
+		return this._currency;
+	}
+	public void setCurrency(Unit unit) {
+		this._currency = unit;
+	}
+	private Integer _shares;
+	public void setShares(Integer shares) {
+		this._shares = shares;
+	}
+	public Integer getShares() {
+		return this._shares;
+	}
+	public Fundamental Copy() {
+		return new Fundamental();
+	}
 }
