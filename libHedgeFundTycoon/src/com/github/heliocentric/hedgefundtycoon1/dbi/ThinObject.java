1a39330f6b2513749450663ddbfd2cab9ca57ac5
Dylan Cochran <heliocentric@gmail.com>
2013-11-06 07:34:25 -0500

    Implement some functions

--- libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/dbi/ThinObject.java
+++ libHedgeFundTycoon/src/com/github/heliocentric/hedgefundtycoon1/dbi/ThinObject.java
@@ -9,5 +9,16 @@ package com.github.heliocentric.hedgefundtycoon1.dbi;
  * @author Owner
  */
 public abstract class ThinObject implements IThinObject {
+	public ThinObject(){
+		
+	}
+	private Database DB;
+	@Override
+	public void setDB(Database DB) {
 	
+	}
+	@Override
+	public Database getDB() {
+		return this.DB;
+	}
 }
